35bcc530ca80ed66d7146943be246775bc7b5935
tasuten <heliopause64@gmail.com>
2017-05-19 13:02:30 +0900

    version option

--- multty.c
+++ multty.c
@@ -1,5 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define NAME "multty"
+#define VERSION "alpha"
+
+
+void usage(void) {
+  fprintf(stderr, "usage: "NAME" [-v]\n");
+  exit(EXIT_FAILURE);
+}
 
 int main(int argc, char *argv[]) {
-  printf("Let's start.\n");
+  int opt;
+  while ((opt = getopt(argc, argv, "v")) != -1) {
+    switch (opt) {
+      case 'v':
+        puts(NAME " " VERSION);
+        exit(EXIT_SUCCESS);
+      default:
+        usage();
+    }
+  }
 }
