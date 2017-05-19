d94d5c7652aca8059b64be42867e77b49cb2efff
tasuten <heliopause64@gmail.com>
2017-05-19 14:58:33 +0900

    forkpty

--- tty.c
+++ tty.c
@@ -6,6 +6,10 @@ tty_t tty_spawn(void) {
     .pid = -1,
   };
 
+  result.pid = forkpty(&result.master_fd, NULL, NULL, NULL);
+  if ( result.pid == -1 ) {
+    fprintf(stderr, "forkpty() dead with errno: %d\n", errno);
+    exit(EXIT_FAILURE);
+  }
   return result;
-
 }
--- tty.h
+++ tty.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
 #if defined(__APPLE__)
 #include <util.h>
 #else
@@ -7,7 +11,7 @@
 #endif
 
 typedef struct _tty {
-  int master_fd; 
+  int master_fd;
   int pid;
 } tty_t;
 
