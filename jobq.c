8a1cd8c07adc2c75446fd5cc951f82a2da903850
tasuten <heliopause64@gmail.com>
2017-05-21 16:19:42 +0900

    jobqueue

---
+++ jobq.c
@@ -0,0 +1,37 @@
+#include "jobq.h"
+
+int* jobq_open(void) {
+  int* q = calloc(2, sizeof(int));
+  if (pipe(q) < 0) {
+    fprintf(stderr, "Opening pipe failed: %s", strerror(errno));
+    return NULL;
+  }
+
+  return q;
+}
+
+void jobq_close(int* queue) {
+  close(queue[READ]);
+  close(queue[WRITE]);
+  free(queue);
+}
+
+packet_t jobq_recv(int* queue) {
+  packet_t pkt;
+  ssize_t readlen = read(queue[READ], &pkt, sizeof(pkt));
+  if (readlen != sizeof(pkt)) {
+    fprintf(stderr, "reading jobqueue failed: %s", strerror(errno));
+    jobq_close(queue);
+    exit(EXIT_FAILURE);
+  }
+  return pkt;
+}
+
+void jobq_send(int* queue, packet_t pkt) {
+  ssize_t writelen = write(queue[WRITE], &pkt, sizeof(pkt));
+  if (writelen != sizeof(pkt)) {
+    fprintf(stderr, "writing jobqueue failed: %s", strerror(errno));
+    jobq_close(queue);
+    exit(EXIT_FAILURE);
+  }
+}
---
+++ jobq.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define READ 0
+#define WRITE 1
+#define PAYLOAD_LEN 256
+
+typedef enum __type {
+  MESSAGE
+} packet_type;
+
+typedef struct __packet {
+  packet_type type;
+  char payload[PAYLOAD_LEN];
+} packet_t;
+
+int* jobq_open(void);
+void jobq_close(int* queue);
+packet_t jobq_recv(int* queue);
+void jobq_send(int* queue, packet_t pkt);
