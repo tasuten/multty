0ad6c4bdc96153776c49889d50635ac978a9d8b4
tasuten <heliopause64@gmail.com>
2017-05-27 20:24:10 +0900

    It become too complex....Rewrite!

--- session.h
+++ session.h
@@ -1,20 +1,4 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-#include <pthread.h>
-
-#include "jobq.h"
-#include "signal_handle.h"
-#include "session_t.h"
-
-
-
 void session_start(void);
 
-void* stdin_handler(void *self);
-void* tty_handler(void *self);
-void* consume_queue(void *self);
