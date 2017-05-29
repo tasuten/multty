3b742313ee5ef330af014e3315acc09fc30e4ddc
tasuten <heliopause64@gmail.com>
2017-05-30 02:17:12 +0900

    session initalizer/closer

--- session.c
+++ session.c
@@ -1,17 +1,32 @@
 #include "session.h"
 
 void session_start(void) {
-  session_t* self = calloc(1, sizeof(session_t));
-  self->tabs_head = tab_new(NULL);
-  self->active = self->tabs_head;
-  self->jobq = jobq_open();
 
+  session_t* self = session_init();
   init_sighandle();
 
   pthread_t sig;
   pthread_create(&sig, NULL, &sighandle_loop, (void *)self);
 
-  while(1);
+  session_close(self);
 
 }
 
+
+session_t* session_init(void) {
+  session_t* self = calloc(1, sizeof(session_t));
+  self->tabs_head = tab_new(NULL);
+  self->active = self->tabs_head;
+  self->jobq = jobq_open();
+  return self;
+}
+
+void session_close(session_t* s) {
+
+  // close all tabs
+  for (tab_t* iter = s->tabs_head; iter != NULL; iter = tab_close(iter));
+  s->active = NULL;
+
+  jobq_close(s->jobq);
+  free(s);
+}
--- session.h
+++ session.h
@@ -5,6 +5,9 @@
 #include "sighandle.h"
 #include "tab.h"
 #include "jobq.h"
+#include "session_t.h"
 
 void session_start(void);
+session_t* session_init(void);
+void session_close(session_t* s);
 
